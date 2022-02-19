f54572e4ab9476914263f0594d60dcb5f4489ed2
산체스 <xognsl87@gmail.com>
2022-02-20 05:13:21 +0900

    feat: setup xcode WalkWithHaesook

---
+++ WalkWithHaesook/WalkWithHaesook/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WalkWithHaesook
+//
+//  Created by tae hoon park on 2022/02/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
