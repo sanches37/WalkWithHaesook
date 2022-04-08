f72548a120a35a3026d6aeaaf66850c529b926fe
산체스 <xognsl87@gmail.com>
2022-04-08 18:13:26 +0900

    refactor: 최소지원버전 ios13.0 -> ios14.0으로 변경 앱딜리게이트 및 신딜리게이트 제거 후 앱을 시작할 WalkWithHaesookApp 타입 생성

---
+++ WalkWithHaesook/WalkWithHaesook/WalkWithHaesook.swift
@@ -0,0 +1,24 @@
+//
+//  WalkWithHaesook.swift
+//  WalkWithHaesook
+//
+//  Created by tae hoon park on 2022/04/08.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct WalkWithHaesookApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            let mapViewModel = MapViewModel()
+            MapContentView(mapViewModel: mapViewModel)
+                .environmentObject(DetailViewModel(mapViewModel: mapViewModel))
+        }
+    }
+}
