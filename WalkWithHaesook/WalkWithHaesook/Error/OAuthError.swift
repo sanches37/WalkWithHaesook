ba28adf3259779aab739f26085604c1b636a77de
sanches37 <xognsl87@gmail.com>
2022-11-12 21:00:57 +0900

    KakaoAuthManager를 다른 타입에서 사용할 수 있는 방식으로 코드 변경

--- WalkWithHaesook/WalkWithHaesook/Error/OAuthError.swift
+++ WalkWithHaesook/WalkWithHaesook/Error/OAuthError.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 enum OAuthError: Error, LocalizedError {
-  case loginFailed(description: String)
-  case tokenLoopupFailed
+  case unknown(description: String)
+  case tokenLookupFailed
   
   var errorDescription: String {
     switch self {
-    case .loginFailed(description: let description):
+    case .unknown(description: let description):
       return "에러: \(description)"
-    case .tokenLoopupFailed:
+    case .tokenLookupFailed:
       return "전달 받을 데이터가 없습니다."
     }
   }
--- WalkWithHaesook/WalkWithHaesook/Manager/KakaoAuthManager.swift
+++ WalkWithHaesook/WalkWithHaesook/Manager/KakaoAuthManager.swift
@@ -10,26 +10,57 @@ import KakaoSDKUser
 import KakaoSDKAuth
 
 struct KaKaoAuthManager {
-  func kakaoLogin() {
+  func getKakaoToken(
+    completion: @escaping (Result<OAuthToken, OAuthError>) -> Void) {
+      guard let getToken = checkedKakaoInstallState() else { return }
+      if AuthApi.hasToken() {
+        UserApi.shared.logout { error in
+          if let error = error {
+            completion(
+              .failure(
+                .unknown(description: error.localizedDescription)
+              )
+            )
+          }
+          completion(.success(getToken))
+        }
+      } else {
+        completion(.success(getToken))
+      }
+    }
+  
+  private func checkedKakaoInstallState() -> OAuthToken? {
+    var result: OAuthToken?
     if UserApi.isKakaoTalkLoginAvailable() {
       UserApi.shared.loginWithKakaoTalk { oAuthToken, error in
-        try? self.onReceiveKakaoToken(oAuthToken: oAuthToken, error: error)
+        do {
+          result = try self.onReceiveKakaoToken(oAuthToken: oAuthToken, error: error)
+        } catch {
+          debugPrint(error)
+        }
       }
     } else {
       UserApi.shared.loginWithKakaoAccount { oAuthToken, error in
-        try? self.onReceiveKakaoToken(oAuthToken: oAuthToken, error: error)
+        do {
+          result = try self.onReceiveKakaoToken(oAuthToken: oAuthToken, error: error)
+        } catch {
+          debugPrint(error)
+        }
       }
     }
+    return result
   }
   
-  private func onReceiveKakaoToken(oAuthToken: OAuthToken?, error: Error?) throws {
+  private func onReceiveKakaoToken(oAuthToken: OAuthToken?, error: Error?) throws -> OAuthToken {
     if let error = error {
-      throw OAuthError.loginFailed(description: error.localizedDescription)
+      throw OAuthError.unknown(
+        description: error.localizedDescription)
     } else {
       guard let kakaoToken = oAuthToken else {
         throw OAuthError.tokenLookupFailed
       }
       print("카카오톡 로그인 성공: \(kakaoToken)")
+      return kakaoToken
     }
   }
 }
